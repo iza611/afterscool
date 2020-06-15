a63c633eceae4e1c9da99936911774d72854fb49
= <niemam10@gmail.com>
2020-06-15 20:30:24 +0200

    This is our first app in React
    We add some buttons and images :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import img_pony from './res/images/pony.png';
+import img_tunk from './res/images/tunk.jpg';
+import img_cat from './res/images/kot.jpeg';
+
+const images = [
+    img_pony,
+    img_tunk,
+    img_cat,
+];
+
+const taged_images = images.map(img => `<img id="image" src=${img} width="300px" />`);
+
 function App() {
-  return (
+function display_img(index){
+    console.log(index);
+    let x = document.getElementById("images");
+    if (x === null){
+        console.log("ERROR x is null");
+        return
+    }
+    x.innerHTML = taged_images[index];
+}
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div id="buttons">
+            <input type="button" value="DESTROY ANIMALS!!" onClick={() => display_img(0)}/>
+            <br/>
+            <input type="button" value="tUR tUR" onClick={() => display_img(1)} />
+            <br/>
+            <input type="button" value="Saszetkaaaaa :D" onClick={() => display_img(2)} />
+        </div>
+        
+        <div id="images">
+            
+        </div>
     </div>
-  );
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//import img_pony from './res/images/pony.png';
+//import img_tunk from './res/images/tunk.jpg';
+//import img_cat from './res/images/kot.jpeg';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//const images = [
+//    img_pony,
+//    img_tunk,
+//    img_cat,
+//];
+//
+//const taged_images = images.map(img => `<img id="image" src=${img} width="300px" />`);
+//
+//let x = document.getElementById("images");
+//x.innerHTML = taged_images[0];
+
